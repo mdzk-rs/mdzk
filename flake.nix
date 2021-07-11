47b60864900fa5c13652c466fc927a90f31074a8
Victor Freire <victor@freire.dev.br>
2021-07-10 22:14:57 -0300

    refactored flake.nix

--- flake.nix
+++ flake.nix
@@ -1,33 +1,42 @@
 {
+  description = "Plain text Zettelkasten based on mdBook";
+
   inputs = {
     utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nmattia/naersk";
   };
 
   outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages."${system}";
-        naersk-lib = naersk.lib."${system}";
-        name = "mdzk";
-      in
-      rec {
-        # `nix build`
-        packages.${name} = naersk-lib.buildPackage {
-          pname = name;
-          root = ./.;
-        };
-        defaultPackage = packages.${name};
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          name = "mdzk";
 
-        # `nix run`
-        apps.${name} = utils.lib.mkApp {
-          drv = packages.${name};
-        };
-        defaultApp = apps.${name};
+          pkgs = import nixpkgs {
+            inherit system;
+          };
 
-        # `nix develop`
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo ];
-        };
-      });
+          naersk-lib = naersk.lib."${system}";
+
+          mdzk-pkg = naersk-lib.buildPackage {
+            pname = name;
+            root = ./.;
+          };
+        in
+        rec {
+          # `nix build`
+          packages.${name} = mdzk-pkg;
+          defaultPackage = packages.${name};
+
+          # `nix run`
+          apps.${name} = utils.lib.mkApp {
+            drv = packages.${name};
+          };
+          defaultApp = apps.${name};
+
+          # `nix develop`
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ rustc cargo ];
+          };
+        });
 }
