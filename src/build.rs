17fb9cca2ba06e81d94377cdc877032bbc0be469
kmaasrud <km@aasrud.com>
2021-08-05 11:28:33 +0200

    Fixed serve.rs and utils.rs, and bumped to v0.2.0

--- src/build.rs
+++ src/build.rs
@@ -1,16 +1,22 @@
-use crate::utils::find_zk_root;
+use crate::utils::{find_zk_root, update_summary};
 use std::path::PathBuf;
-use std::io::prelude::*;
-use std::fs::File;
 use mdbook::{MDBook, Config, errors::Error};
 use mdbook_katex::KatexProcessor;
 use mdbook_backlinks::Backlinks;
 use mdbook_wikilink::WikiLinks;
-use walkdir::WalkDir;
-
-const PAD_SIZE: usize = 4;
 
 pub fn build(dir: Option<PathBuf>) -> Result<(), Error> {
+    let mut zk = init_zk(dir)?;
+
+    zk.with_preprocessor(KatexProcessor);
+    zk.with_preprocessor(Backlinks);
+    zk.with_preprocessor(WikiLinks);
+    zk.build().expect("Builing failed");
+
+    Ok(())
+}
+
+pub fn init_zk(dir: Option<PathBuf>) -> Result<MDBook, Error> {
     let path = match dir {
         Some(path) => path,
         None => find_zk_root().ok_or(Error::msg("Could not find the root of your Zettelkasten"))?,
@@ -20,45 +26,7 @@ pub fn build(dir: Option<PathBuf>) -> Result<(), Error> {
     let config: Config = Config::from_disk(config_path)?;
 
     let book_source = &config.book.src;
-    let summary = WalkDir::new(book_source)
-        .sort_by_file_name()
-        .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(|e| e.path() != book_source)
-        .map(|e| {
-            let stripped_path = e.path().strip_prefix(&book_source).unwrap();
-            let file_stem = stripped_path.file_stem().unwrap().to_str().unwrap();
-            let depth = (stripped_path.components().count() - 1) * PAD_SIZE;
+    update_summary(&book_source)?;
 
-            if e.path().is_dir() {
-                return Some(format!("{1:>0$}- [{2}]()\n", depth, "", file_stem));
-            }
-
-            let file_ext = e.path().extension().unwrap().to_str().unwrap();
-            if file_ext == "md" {
-                return Some(format!(
-                    "{1:>0$}- [{2}](<{3}>)\n",
-                    depth,
-                    "",
-                    file_stem,
-                    stripped_path.to_str().unwrap()
-                ));
-            } else {
-                None
-            }
-        })
-        .filter_map(|e| e)
-        .fold(String::new(), |acc, curr| acc + &curr);
-
-    let mut summary_file = File::create([&config.book.src, &PathBuf::from("SUMMARY.md")].iter().collect::<PathBuf>())?;
-    summary_file.write_all(summary.as_bytes())?;
-
-    let mut zk = MDBook::load_with_config(path, config)?;
-
-    zk.with_preprocessor(KatexProcessor);
-    zk.with_preprocessor(Backlinks);
-    zk.with_preprocessor(WikiLinks);
-    zk.build().expect("Builing failed");
-
-    Ok(())
+    Ok(MDBook::load_with_config(path, config)?)
 }
--- src/utils.rs
+++ src/utils.rs
@@ -6,11 +6,17 @@ use std::{
     process::Command,
     env,
 };
+use walkdir::WalkDir;
+use std::fs::File;
+use std::io::prelude::*;
+use mdbook::errors::Error;
 
-/// Search up the filesystem to find a book.toml file and return it's parent directory.
+const PAD_SIZE: usize = 4;
+
+/// Search up the filesystem to find a zk.toml file and return it's parent directory.
 pub fn find_zk_root() -> Option<PathBuf> {
     let mut path: PathBuf = env::current_dir().unwrap().into();
-    let file = Path::new("book.toml");
+    let file = Path::new("zk.toml");
 
     loop {
         path.push(file);
@@ -39,4 +45,40 @@ pub fn get_author_name() -> Option<String> {
     } else {
         None
     }
+}
+
+pub fn update_summary(book_source: &PathBuf) -> Result<(), Error> {
+    let summary = WalkDir::new(book_source)
+        .sort_by_file_name()
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.path() != book_source)
+        .map(|e| {
+            let stripped_path = e.path().strip_prefix(&book_source).unwrap();
+            let file_stem = stripped_path.file_stem().unwrap().to_str().unwrap();
+            let depth = (stripped_path.components().count() - 1) * PAD_SIZE;
+
+            if e.path().is_dir() {
+                return Some(format!("{1:>0$}- [{2}]()\n", depth, "", file_stem));
+            }
+
+            let file_ext = e.path().extension().unwrap().to_str().unwrap();
+            if file_ext == "md" {
+                return Some(format!(
+                    "{1:>0$}- [{2}](<{3}>)\n",
+                    depth,
+                    "",
+                    file_stem,
+                    stripped_path.to_str().unwrap()
+                ));
+            } else {
+                None
+            }
+        })
+        .filter_map(|e| e)
+        .fold(String::new(), |acc, curr| acc + &curr);
+
+    let mut summary_file = File::create([book_source, &PathBuf::from("SUMMARY.md")].iter().collect::<PathBuf>())?;
+    summary_file.write_all(summary.as_bytes())?;
+    Ok(())
 }
