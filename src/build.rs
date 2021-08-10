55606a523286c542dd9d3db02657c104df34b84d
Xiangru Lian <admin@mail.xrlian.com>
2021-08-10 13:40:02 -0700

    feat: support disabling default preprocessors

--- src/build.rs
+++ src/build.rs
@@ -7,6 +7,7 @@ use mdbook_backlinks::Backlinks;
 use mdbook_katex::KatexProcessor;
 use mdbook_wikilink::WikiLinks;
 use std::path::PathBuf;
+use toml::Value;
 
 pub fn build(dir: Option<PathBuf>) -> Result<(), Error> {
     let zk = init_zk(dir)?;
@@ -29,10 +30,13 @@ pub fn init_zk(dir: Option<PathBuf>) -> Result<MDBook, Error> {
     update_summary(&book_source)?;
 
     let mut zk = MDBook::load_with_config(path, config)?;
-    zk.with_preprocessor(FrontMatter);
-    zk.with_preprocessor(KatexProcessor);
-    zk.with_preprocessor(Backlinks);
-    zk.with_preprocessor(WikiLinks);
+    if !zk.config.get("disable_default_preprocessors").unwrap_or(&Value::Boolean(false))
+        .as_bool().ok_or(Error::msg("disable_default_preprocessors should be a boolean"))? {
+        zk.with_preprocessor(FrontMatter);
+        zk.with_preprocessor(KatexProcessor);
+        zk.with_preprocessor(Backlinks);
+        zk.with_preprocessor(WikiLinks);
+    }
 
     Ok(zk)
 }
