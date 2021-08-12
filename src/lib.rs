5f93e5b71320ff6a59335ca7a092f8b5595c5128
kmaasrud <km@aasrud.com>
2021-08-12 19:10:18 +0200

    Decided on global variables

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,8 @@ pub mod watch;
 
 pub use crate::{build::build, init::init, serve::serve};
 
-pub const SRC_DIR: &str = "zettels";
-pub const BUILD_DIR: &str = "out";
+pub const SRC_DIR: &str = "notes";
+pub const BUILD_DIR: &str = "html";
 pub const CONFIG_FILE: &str = "mdzk.toml";
 pub const SUMMARY_FILE: &str = ".mdzk_summary.md";
 pub const DEFAULT_ZK_TITLE: &str = "My mdzk";
