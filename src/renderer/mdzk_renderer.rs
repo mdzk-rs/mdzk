51c1b92630cc042b05811571c6ef48e76b8e7297
kmaasrud <km@aasrud.com>
2021-08-21 18:26:09 +0200

    Simple HTML generation of vault

--- src/renderer/mdzk_renderer.rs
+++ src/renderer/mdzk_renderer.rs
@@ -1,10 +1,32 @@
-use mdbook::{Renderer, renderer::RenderContext, errors::*, utils, BookItem};
+use crate::utils;
+use mdbook::{Renderer, renderer::RenderContext, errors::*, BookItem, book::Chapter};
 use anyhow::Context;
 use std::fs;
+use pulldown_cmark::{Options, Parser, html::push_html};
 
 /// The HTML backend for mdzk, implementing [`Renderer`](https://docs.rs/mdbook/0.4.12/mdbook/renderer/trait.Renderer.html).
 #[derive(Default)]
 pub struct HtmlMdzk;
+
+impl HtmlMdzk {
+    fn render_chapter(&self, ch: &Chapter, ctx: &RenderContext) -> Result<()> {
+        let mut opts = Options::empty();
+        opts.insert(Options::ENABLE_TABLES);
+        opts.insert(Options::ENABLE_FOOTNOTES);
+        opts.insert(Options::ENABLE_STRIKETHROUGH);
+        opts.insert(Options::ENABLE_TASKLISTS);
+        let parser = Parser::new_ext(&ch.content, opts);
+
+        let mut html = String::new();
+        push_html(&mut html, parser);
+
+        let path = &ctx.destination.join(ch.path.as_ref().unwrap().with_extension("html"));
+        debug!("Writing {:?}", &ctx.destination.join(&path));
+        utils::write_file(path, html.as_bytes())?;
+
+        Ok(())
+    }
+}
 
 impl Renderer for HtmlMdzk {
     fn name(&self) -> &str {
@@ -16,22 +38,21 @@ impl Renderer for HtmlMdzk {
         let book = &ctx.book;
 
         if destination.exists() {
-            utils::fs::remove_dir_content(destination)
-                .with_context(|| "Unable to remove stale Markdown output")?;
+            // If output directory exists already, remove all content inside
+            mdbook::utils::fs::remove_dir_content(destination)
+                .with_context(|| "Unable to remove old output")?;
         } else {
+            // If output direcory doesn't exist, create it
             fs::create_dir_all(&destination)
                 .with_context(|| "Unexpected error when constructing destination path")?;
         }
 
-        trace!("markdown render");
+        // Render out each note
+        trace!("mdzk render");
         for item in book.iter() {
             if let BookItem::Chapter(ref ch) = *item {
                 if !ch.is_draft_chapter() {
-                    utils::fs::write_file(
-                        &ctx.destination,
-                        &ch.path.as_ref().expect("Checked path exists before"),
-                        ch.content.as_bytes(),
-                    )?;
+                    self.render_chapter(ch, ctx)?;
                 }
             }
         }
