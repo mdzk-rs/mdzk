1c762849cbfc1d708b44decc126f78007e86f48f
kmaasrud <km@aasrud.com>
2021-08-29 12:04:17 +0200

    Basic handlebars support for mdzk renderer (#6)

--- src/renderer/mdzk_renderer.rs
+++ src/renderer/mdzk_renderer.rs
@@ -2,14 +2,17 @@ use crate::utils;
 use mdbook::{Renderer, renderer::RenderContext, errors::*, BookItem, book::Chapter};
 use anyhow::Context;
 use std::fs;
+use std::collections::BTreeMap;
 use pulldown_cmark::{Options, Parser, html::push_html};
+use handlebars::{Handlebars, no_escape};
 
 /// The HTML backend for mdzk, implementing [`Renderer`](https://docs.rs/mdbook/0.4.12/mdbook/renderer/trait.Renderer.html).
 #[derive(Default)]
 pub struct HtmlMdzk;
 
 impl HtmlMdzk {
-    fn render_chapter(&self, ch: &Chapter, ctx: &RenderContext) -> Result<()> {
+    fn render_chapter(&self, ch: &Chapter, ctx: &RenderContext, hbs: &Handlebars) -> Result<()> {
+        // Set up CommonMark parser
         let mut opts = Options::empty();
         opts.insert(Options::ENABLE_TABLES);
         opts.insert(Options::ENABLE_FOOTNOTES);
@@ -17,12 +20,21 @@ impl HtmlMdzk {
         opts.insert(Options::ENABLE_TASKLISTS);
         let parser = Parser::new_ext(&ch.content, opts);
 
-        let mut html = String::new();
-        push_html(&mut html, parser);
+        // Push HTML into string
+        let mut content = String::new();
+        push_html(&mut content, parser);
 
+        // Make map with values for all handlebars keys
+        let mut data = BTreeMap::new();
+        data.insert("content", &content);
+
+        // Render output
+        let out = hbs.render("index", &data)?;
+        
+        // Write to file
         let path = &ctx.destination.join(ch.path.as_ref().unwrap().with_extension("html"));
         debug!("Writing {:?}", &ctx.destination.join(&path));
-        utils::write_file(path, html.as_bytes())?;
+        utils::write_file(path, out.as_bytes())?;
 
         Ok(())
     }
@@ -43,16 +55,20 @@ impl Renderer for HtmlMdzk {
                 .with_context(|| "Unable to remove old output")?;
         } else {
             // If output direcory doesn't exist, create it
-            fs::create_dir_all(&destination)
+            fs::create_dir_all(destination)
                 .with_context(|| "Unexpected error when constructing destination path")?;
         }
+
+        let mut hbs = Handlebars::new();
+        hbs.register_escape_fn(no_escape);
+        hbs.register_template_string("index", include_str!("theme/index.hbs"))?;
 
         // Render out each note
         trace!("mdzk render");
         for item in book.iter() {
             if let BookItem::Chapter(ref ch) = *item {
                 if !ch.is_draft_chapter() {
-                    self.render_chapter(ch, ctx)?;
+                    self.render_chapter(ch, ctx, &hbs)?;
                 }
             }
         }
