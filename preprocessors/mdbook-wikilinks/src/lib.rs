13b47d1136601cdfa756dc1c428b7fa7f6ae000d
kmaasrud <km@aasrud.com>
2021-10-24 23:32:55 +0200

    Basic parsing of link internals

--- preprocessors/mdbook-wikilinks/src/lib.rs
+++ preprocessors/mdbook-wikilinks/src/lib.rs
@@ -1,3 +1,7 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
 use lazy_regex::{lazy_regex, Lazy};
 use mdbook::{
     book::{Book, Chapter},
@@ -5,9 +9,14 @@ use mdbook::{
     preprocess::{CmdPreprocessor, Preprocessor, PreprocessorContext},
     BookItem,
 };
+use pest::Parser;
 use regex::{Captures, Regex};
 use std::{collections::HashMap, io, path::PathBuf};
-use pulldown_cmark::{CowStr, Event, Parser};
+use pulldown_cmark::{CowStr, Event, escape::escape_href};
+
+#[derive(Parser)]
+#[grammar = "wikilink.pest"]
+pub struct WikiLinkParser;
 
 static WIKILINK_REGEX: Lazy<Regex> =
     lazy_regex!(r"\[\[(?P<link>[^\]\|]+)(?:\|(?P<title>[^\]]+))?\]\]");
@@ -48,25 +57,53 @@ impl Preprocessor for WikiLinks {
     }
 
     fn run(&self, _ctx: &PreprocessorContext, mut book: Book) -> Result<Book, Error> {
-        let mut chapters_map = HashMap::new();
+        let mut path_map = HashMap::new();
         for chapter in book.iter().filter_map(chapter) {
             let key = chapter.name.clone();
             if chapter.path.is_none() {
                 continue;
             }
-            if chapters_map.contains_key(&key) {
+            if path_map.contains_key(&key) {
                 eprintln!("duplicated page title found: {} at {:?}", key, chapter.path);
             }
-            chapters_map.insert(key, chapter.path.as_ref().unwrap().clone());
+            path_map.insert(key, chapter.path.as_ref().unwrap().clone());
         }
 
         book.for_each_mut(|it| {
             if let BookItem::Chapter(chapter) = it {
                 pulldown_method(&chapter.content.clone(), |link_text| {
-                    chapter.content = chapter.content.replacen(&format!("[[{}]]", link_text), "!!This was link!!", 1);
+                    let mut link = WikiLinkParser::parse(Rule::link, link_text)
+                        .expect("Unsuccessful parse")
+                        .next()
+                        .unwrap()
+                        .into_inner();
+
+                    let dest = link.next().unwrap().into_inner().next().unwrap().as_str();
+                    let href = if !path_map.contains_key(dest) {
+                        format!(
+                            "<span class=\"missing-link\" style=\"color:darkred;\">{}</span>", 
+                            dest
+                        )
+                    } else {
+                        pathdiff::diff_paths(
+                            path_map.get(dest).unwrap(),
+                            chapter.path.as_ref().unwrap().parent().unwrap(),
+                        ).unwrap().to_string_lossy().to_string() // Gotta love Rust <3
+                    };
+
+                    let title = match link.next() {
+                        Some(alias) => alias.as_str(),
+                        None => dest.as_ref(),
+                    };
+
+                    chapter.content = chapter.content.replacen(
+                        &format!("[[{}]]", link_text),
+                        &format!("[{}](<{}>)", title, escape_special_chars(&href)),
+                        1
+                    );
                 });
 
-                // chapter.content = regex_method(&chapter, &chapters_map);
+                // chapter.content = _regex_method(&chapter, &path_map);
             }
         });
 
@@ -74,36 +111,6 @@ impl Preprocessor for WikiLinks {
     }
 }
 
-fn regex_method(chapter: &Chapter, chapters_map: &HashMap<String, PathBuf>) -> String {
-    WIKILINK_REGEX
-        .replace_all(&chapter.content, |it: &Captures| -> String {
-            let key = it.name("link").unwrap().as_str().trim();
-            if !chapters_map.contains_key(key) {
-                return (maud::html! {
-                    span.missing-link style="color:darkred;" {
-                       (key)
-                    }
-                })
-                .into_string();
-            }
-            let title = it
-                .name("title")
-                .map(|x| x.as_str().trim().to_string())
-                .unwrap_or(key.to_string());
-            let diff_path = pathdiff::diff_paths(
-                chapters_map.get(key).unwrap(),
-                chapter.path.as_ref().unwrap().parent().unwrap(),
-            )
-            .unwrap();
-
-            return format!(
-                "[{}](<{}>)",
-                title,
-                escape_special_chars(&diff_path.to_string_lossy())
-            );
-        })
-        .to_string()
-}
 
 fn pulldown_method(content: &str, mut handle_link: impl FnMut(&str)) {
     enum Currently {
@@ -114,7 +121,7 @@ fn pulldown_method(content: &str, mut handle_link: impl FnMut(&str)) {
         Ignore,
     }
 
-    let parser = Parser::new(content);
+    let parser = pulldown_cmark::Parser::new(content);
 
     let mut buffer = String::new();
     let mut current = Currently::OutsideLink;
@@ -171,12 +178,44 @@ fn pulldown_method(content: &str, mut handle_link: impl FnMut(&str)) {
     }
 }
 
+fn _regex_method(chapter: &Chapter, chapters_map: &HashMap<String, PathBuf>) -> String {
+    WIKILINK_REGEX
+        .replace_all(&chapter.content, |it: &Captures| -> String {
+            let key = it.name("link").unwrap().as_str().trim();
+            if !chapters_map.contains_key(key) {
+                return format!(
+                    "<span class=\"missing-link\" style=\"color:darkred;\">{}</span>", 
+                    key
+                )
+            }
+            let title = it
+                .name("title")
+                .map(|x| x.as_str().trim().to_string())
+                .unwrap_or(key.to_string());
+            let diff_path = pathdiff::diff_paths(
+                chapters_map.get(key).unwrap(),
+                chapter.path.as_ref().unwrap().parent().unwrap(),
+            )
+            .unwrap();
+
+            return format!(
+                "[{}](<{}>)",
+                title,
+                escape_special_chars(&diff_path.to_string_lossy())
+            );
+        })
+        .to_string()
+}
+
 /// Escape characters for usage in URLs
 fn escape_special_chars(text: &str) -> String {
-    text.replace(" ", "%20")
+    let mut buf = String::new();
+    escape_href(&mut buf, text).ok();
+    buf
+    /* text.replace(" ", "%20")
         .replace("<", "%3C")
         .replace(">", "%3E")
-        .replace('?', "%3F")
+        .replace('?', "%3F") */
 }
 
 #[cfg(test)]
