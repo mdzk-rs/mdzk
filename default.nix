89b0789c5c1f64bbd030c6407f9034cd5b0c0fb8
Victor Freire <victor@freire.dev.br>
2021-10-27 09:38:04 -0300

    added CoreServices

--- default.nix
+++ default.nix
@@ -5,6 +5,7 @@ pkgs.rustPlatform.buildRustPackage {
 
   src = pkgs.lib.cleanSource ./.;
 
+  buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.CoreServices ]; 
   nativeBuildInputs = [ rust-toolchain ];
 
   cargoSha256 = "sha256-kfqsqFuDNgYMAQM2AD8tliW0norvBrcRQtrwzJRw8tY=";
