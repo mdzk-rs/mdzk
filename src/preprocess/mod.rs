93c416004f5e50d1bc568b5c11e8bcdf5cb40049
kmaasrud <km@aasrud.com>
2021-11-21 09:58:00 +0100

    Moved backlinks logic over to monopreprocessor

--- src/preprocess/mod.rs
+++ src/preprocess/mod.rs
@@ -3,6 +3,8 @@ mod katex;
 pub mod readme;
 mod wikilinks;
 
+use crate::utils::diff_paths;
+
 use anyhow::Result;
 use lazy_regex::regex;
 use mdbook::{
@@ -51,6 +53,7 @@ impl Preprocessor for MdzkPreprocessor {
 
         // First iteration
         let mut path_map = HashMap::new();
+        let mut backlinks_map = HashMap::new();
         book.for_each_mut(|item| {
             if let BookItem::Chapter(ch) = item {
                 // Handle front matter
@@ -67,24 +70,26 @@ impl Preprocessor for MdzkPreprocessor {
                     )
                 }
 
-                // Populate index of all paths for use with wikilinks
+                // Populate path_map with note names and paths.
+                // Populate backlinks_map with note paths and an empty vector.
                 if ch.path.is_some() {
                     let key = ch.name.clone();
+                    let path = ch.path.as_ref().unwrap().to_owned();
                     if path_map.contains_key(&key) {
                         warn!(
                             r#"Duplicated page title found:
 
 - {} ({:?})
-- {} ({:?})
+- {0} ({:?})
 
 If links do not properly specify paths, they might lead to the wrong note..."#,
                             key,
-                            ch.path,
-                            key,
+                            path,
                             path_map.get(&key),
                         );
                     }
-                    path_map.insert(key, ch.path.as_ref().unwrap().clone());
+                    path_map.insert(key, path.clone());
+                    backlinks_map.insert(path, Vec::new());
                 }
             }
         });
@@ -104,6 +109,12 @@ If links do not properly specify paths, they might lead to the wrong note..."#,
                                 }
                             };
 
+                        if let Some(ref path) = wikilink.path {
+                            if let Some(backlinks) = backlinks_map.get_mut(path) {
+                                backlinks.push((ch.path.clone().unwrap(), ch.name.clone()));
+                            }
+                        }
+
                         ch.content = ch.content.replacen(
                             &format!("[[{}]]", link_text),
                             &wikilink.cmark(ch.path.as_ref().unwrap().parent().unwrap()),
@@ -115,6 +126,28 @@ If links do not properly specify paths, they might lead to the wrong note..."#,
                 // Generate KaTeX spans/divs
                 if math {
                     katex::run(ch);
+                }
+            }
+        });
+
+        // Third iteration
+        book.for_each_mut(|item| {
+            if let BookItem::Chapter(ch) = item {
+                if let Some(path) = &ch.path {
+                    if let Some(backlinks) = backlinks_map.get(path) {
+                        if !backlinks.is_empty() {
+                            // ch.content += &backlink_prefix;
+                            for (dest, name) in backlinks.iter() {
+                                let diff_path = diff_paths(dest, path.parent().unwrap()).unwrap();
+                                ch.content += &format!(
+                                    "> - [{}](<{}>)\n",
+                                    name,
+                                    diff_path.to_string_lossy()
+                                );
+                            }
+                        };
+
+                    }
                 }
             }
         });
