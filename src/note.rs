e52334fa30148f18a88f819d4014e1cedf50396c
kmaasrud <km@aasrud.com>
2022-01-29 12:46:57 +0100

    Parse front matter

--- src/note.rs
+++ src/note.rs
@@ -1,4 +1,7 @@
-use crate::link::Edge;
+use crate::{error::Result, link::Edge};
+use chrono::{DateTime, NaiveDate};
+use gray_matter::{Matter, engine::YAML};
+use serde::Deserialize;
 use std::{
     collections::{hash_map::DefaultHasher, HashMap},
     hash::{Hash, Hasher},
@@ -30,4 +33,39 @@ pub struct Note {
     pub date: Option<chrono::NaiveDateTime>,
     pub content: String,
     pub adjacencies: HashMap<NoteId, Edge>,
+}
+
+impl Note {
+    pub fn process_content(&mut self, content: &str) -> Result<()> {
+        #[derive(Deserialize)]
+        struct FrontMatter {
+            title: Option<String>,
+            tags: Option<Vec<String>>,
+            date: Option<String>,
+        }
+
+        let gray_matter = Matter::<YAML>::new().parse(content);
+
+        if let Some(data) = gray_matter.data {
+            if let Ok(front_matter) = data.deserialize::<FrontMatter>() {
+                if let Some(title) = front_matter.title {
+                    self.title = title;
+                }
+                if let Some(tags) = front_matter.tags {
+                    self.tags = tags;
+                }
+                if let Some(datestring) = front_matter.date {
+                    self.date = DateTime::parse_from_rfc3339(&datestring)
+                        .or_else(|_| DateTime::parse_from_rfc3339(format!("{}Z", datestring).as_ref()))
+                        .map(|s| s.naive_local())
+                        .or_else(|_| NaiveDate::parse_from_str(&datestring, "%Y-%m-%d").map(|s| s.and_hms(0, 0, 0)))
+                        .ok();
+                }
+            }
+        }
+
+        self.content = gray_matter.content;
+
+        Ok(())
+    }
 }
