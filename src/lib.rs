b5d99e6500c0ee11bffeb7a7282aa5647fe7870f
kmaasrud <km@aasrud.com>
2022-01-29 09:30:16 +0100

    Make binary crate and move Config to it
    Might move to it's own repo

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,9 @@
 #[macro_use]
 pub mod log;
 
-mod config;
 mod link;
 mod note;
 mod vault;
 
-pub use config::Config;
 pub use note::{Note, NoteId};
 pub use vault::Vault;
-
-pub const DEFAULT_BUILD_DIR: &str = "html";
-pub const DEFAULT_CONFIG_FILE: &str = "mdzk.toml";
-pub const DEFAULT_SRC_DIR: &str = "notes";
