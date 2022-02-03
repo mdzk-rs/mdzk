8da363a4498027b8ed3e1b9025a59be87f43b7c7
kmaasrud <km@aasrud.com>
2022-02-03 13:48:55 +0100

    Add iterator ease-of-use funcs

--- src/vault/mod.rs
+++ src/vault/mod.rs
@@ -10,8 +10,8 @@ use std::collections::HashMap;
 
 #[derive(Default)]
 pub struct Vault {
-    pub notes: HashMap<NoteId, Note>,
-    pub(crate) id_lookup: HashMap<String, NoteId>,
+    notes: HashMap<NoteId, Note>,
+    id_lookup: HashMap<String, NoteId>,
 }
 
 impl Vault {
@@ -19,19 +19,32 @@ impl Vault {
         self.notes.iter()
     }
 
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = (&NoteId, &mut Note)> + '_ {
+        self.notes.iter_mut()
+    }
+
     pub fn id_of(&self, title_or_path: impl AsRef<str>) -> Option<&NoteId> {
         self.id_lookup.get(title_or_path.as_ref())
     }
 
-    pub fn backlinks(&self, id: &NoteId) -> Vec<Note> {
+    pub fn backlinks(&self, id: &NoteId) -> Vec<NoteId> {
         self.iter()
-            .filter_map(|(_, note)| {
+            .filter_map(|(backlink_id, note)| {
                 if matches!(note.adjacencies.get(&id), Some(Edge::Connected)) {
-                    Some(note.to_owned())
+                    Some(backlink_id.to_owned())
                 } else {
                     None
                 }
             })
             .collect()
+    }
+}
+
+impl IntoIterator for Vault {
+    type Item = (NoteId, Note);
+    type IntoIter = std::collections::hash_map::IntoIter<NoteId, Note>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.notes.into_iter()
     }
 }
